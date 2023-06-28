1c270ef0167fd6cc2971b9872999241bb7dea8a6
vikas <vik.k.shah@gmail.com>
2023-06-28 18:29:32 -0500

    Add Codable models

---
+++ FetchRewards-Interview/Models/Meal.swift
@@ -0,0 +1,14 @@
+//
+//  Meal.swift
+//  FetchRewards-Interview
+//
+//  Created by Vikas Shah on 6/28/23.
+//
+
+import Foundation
+
+struct Meal: Codable {
+    let strMeal: String
+    let strMealThumb: String
+    let idMeal: String
+}
---
+++ FetchRewards-Interview/Models/Menu.swift
@@ -0,0 +1,12 @@
+//
+//  Meals.swift
+//  FetchRewards-Interview
+//
+//  Created by Vikas Shah on 6/28/23.
+//
+
+import Foundation
+
+struct Menu: Codable {
+    let meals: [Meal]
+}
