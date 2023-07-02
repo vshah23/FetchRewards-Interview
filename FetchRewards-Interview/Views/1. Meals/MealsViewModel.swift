f6c7346315f52600964433a49a2c9a9f7ce8954c
vikas <vik.k.shah@gmail.com>
2023-07-02 15:36:19 -0500

    Setup view states

--- FetchRewards-Interview/Views/1. Meals/MealsViewModel.swift
+++ FetchRewards-Interview/Views/1. Meals/MealsViewModel.swift
@@ -6,4 +6,21 @@
 //
 
 import Foundation
+import Combine
 
+enum MealsViewState: Equatable {
+    case loading
+    case loaded
+    case error(String)
+}
+
+class MealsViewModel {
+    private let mealsRepository: MealRepository
+    
+    let state: CurrentValueSubject<MealsViewState, Never> = CurrentValueSubject<MealsViewState, Never>(.loading)
+    let title: String = "Meals"
+
+    init(mealsRepository: MealRepository) {
+        self.mealsRepository = mealsRepository
+    }
+}
