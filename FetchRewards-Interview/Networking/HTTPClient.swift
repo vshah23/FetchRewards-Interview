148638bd289f1397cc1716f675e7e8f125738d15
vikas <vik.k.shah@gmail.com>
2023-07-02 15:42:46 -0500

    HTTPClient implementation

--- FetchRewards-Interview/Networking/HTTPClient.swift
+++ FetchRewards-Interview/Networking/HTTPClient.swift
@@ -29,3 +29,50 @@ protocol HTTPClientSession {
 }
 
 extension URLSession: HTTPClientSession { }
+
+struct HTTPClientImpl: HTTPClient {
+    let session: HTTPClientSession
+    
+    init(session: HTTPClientSession?) {
+        guard let session: HTTPClientSession = session else {
+            self.session = URLSession(configuration: .default)
+            return
+        }
+
+        self.session = session
+    }
+    
+    func performRequest(request: URLRequest, _ completion: @escaping (HTTPClientResult) -> Void) {
+        session.dataTask(with: request, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) -> Void in
+            if let error: URLError = error as? URLError, error.errorCode == NSURLErrorNotConnectedToInternet {
+                completion(.failure(HTTPClientError.offline))
+            } else if let response: HTTPURLResponse = response as? HTTPURLResponse, 200...299 ~= response.statusCode {
+                completion(.success(response))
+            } else {
+                completion(.failure(HTTPClientError.unknown))
+            }
+        }).resume()
+    }
+    
+}
+
+extension HTTPClientImpl {
+    func get(_ url: String, queryParams: [URLQueryItem]?, completion: @escaping (HTTPClientResult) -> Void) {
+        guard var urlComponents: URLComponents = URLComponents(string: url) else {
+            completion(.failure(HTTPClientError.invalidURL))
+            return
+        }
+        
+        urlComponents.queryItems = queryParams
+        
+        guard let url: URL = urlComponents.url else {
+            completion(.failure(HTTPClientError.invalidURL))
+            return
+        }
+        
+        var request: URLRequest = URLRequest(url: url)
+        request.httpMethod = HTTPMethod.GET.rawValue
+        
+        performRequest(request: request, completion)
+    }
+}
