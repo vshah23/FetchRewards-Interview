29bf57b59714c83a69a9711016e1f99b5b5300ed
vikas <vik.k.shah@gmail.com>
2023-07-05 16:24:58 -0500

    Added generated localization strings

---
+++ FetchRewards-Interview/Generated/Strings+Generated.swift
@@ -0,0 +1,78 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+import Foundation
+
+// swiftlint:disable superfluous_disable_command file_length implicit_return prefer_self_in_static_references
+
+// MARK: - Strings
+
+// swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
+internal enum L10n {
+  internal enum Alert {
+    /// OK
+    internal static let defaultAction = L10n.tr("Localizable", "alert.defaultAction", fallback: "OK")
+    /// MealsDB
+    internal static let title = L10n.tr("Localizable", "alert.title", fallback: "MealsDB")
+  }
+  internal enum Loading {
+    internal enum Screen {
+      internal enum Loading {
+        /// Loading...
+        internal static let status = L10n.tr("Localizable", "loading.screen.loading.status", fallback: "Loading...")
+      }
+    }
+  }
+  internal enum Meal {
+    internal enum Details {
+      internal enum Section {
+        internal enum Header {
+          /// Ingredients
+          internal static let ingredients = L10n.tr("Localizable", "meal.details.section.header.ingredients", fallback: "Ingredients")
+          /// Instructions
+          internal static let instructions = L10n.tr("Localizable", "meal.details.section.header.instructions", fallback: "Instructions")
+        }
+      }
+    }
+  }
+  internal enum Meals {
+    internal enum List {
+      /// Fetching recipes...
+      internal static let title = L10n.tr("Localizable", "meals.list.title", fallback: "Fetching recipes...")
+      internal enum Refreshcontrol {
+        /// Fetching recipes...
+        internal static let title = L10n.tr("Localizable", "meals.list.refreshcontrol.title", fallback: "Fetching recipes...")
+      }
+    }
+  }
+  internal enum UserFriendlyError {
+    /// Sorry, something went wrong. Please try again.
+    internal static let generic = L10n.tr("Localizable", "userFriendlyError.generic", fallback: "Sorry, something went wrong. Please try again.")
+    /// Please check your network connection and try again.
+    internal static let offline = L10n.tr("Localizable", "userFriendlyError.offline", fallback: "Please check your network connection and try again.")
+  }
+}
+// swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:enable nesting type_body_length type_name vertical_whitespace_opening_braces
+
+// MARK: - Implementation Details
+
+extension L10n {
+  private static func tr(_ table: String, _ key: String, _ args: CVarArg..., fallback value: String) -> String {
+    let format = BundleToken.bundle.localizedString(forKey: key, value: value, table: table)
+    return String(format: format, locale: Locale.current, arguments: args)
+  }
+}
+
+// swiftlint:disable convenience_type
+private final class BundleToken {
+  static let bundle: Bundle = {
+    #if SWIFT_PACKAGE
+    return Bundle.module
+    #else
+    return Bundle(for: BundleToken.self)
+    #endif
+  }()
+}
+// swiftlint:enable convenience_type
--- README.md
+++ README.md
@@ -16,11 +16,28 @@
 - [x] Unit tests
 - [ ] UI tests
 
-## Architecture:
+## Architecture
 ![app_architecture](/app_architecture.png)
 
-## Room for improvement:
+## Room for improvement
 
 * We can promote a more user friendly app experience by queuing up state change events and handling them with a delay between each event to prevent jarring state changes while loading data in good network conditions. The tools Combine provides out of the box for controlling timing (debounce and throttle) do not help us accomplish this. This stackoverflow post details a potential way to reach our goals: https://stackoverflow.com/a/76590605
 * Allow users to cancel requests
 * Bump code coverage to 80%
+* For the thumbnail images we can download the data per usual in our repository and utilize an NSCache object to cache the images
+* Break the UI up into reusable components
+* The names of the properties in the model can be refactored so they're a bit nicer on the client side. From the API I would prefer if the model returned an array of "ingredients" instead of having separate properties for each name and value.
+
+## Assumptions
+* We need to handle more than just the happy path. A good application is not only defined by how well it performs when everything is going well, but also how well it performs when handling error states.
+
+## Tidbits
+
+* I have a personal preference to explicitly declare types as I find it easier to read code when the types are readily visible.
+* I focused more on the architecture than the UI. I wanted to make sure everything was decoupled and readily testable as I learned that's one of the most important things that can instill confidence when releasing changes.
+* At the time of submission we're sitting at 59% code coverage
+* Pull to refresh is implemented
+* The loading screen is reusable
+* There is a state on the meal list screen for an empty data set
+* There is a state on the meal list screen for displaying an alert in the case of some error ()
+
